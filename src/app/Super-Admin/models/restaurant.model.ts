1a1c248025dd3e37b05334328aaff9174776e2c9
ebgarcia <165815792+eb-gar@users.noreply.github.com>
2025-07-14 21:26:02 -0500

    Forms for selecting the company color and uploading the company logo.
    The frontend was connected to the backend to add form sections for selecting a company's color and uploading its logo. Tests were also carried out to ensure everything worked as expected.

--- src/app/Super-Admin/models/restaurant.model.ts
+++ src/app/Super-Admin/models/restaurant.model.ts
@@ -2,6 +2,8 @@ export interface Restaurant {
   id?: number;
   nombre: string;
   propietario: string;
-  direccion?: string;
-  creadoEn?: string;
+  direccion: string;
+  primaryColor?: string;
+  secondaryColor?: string;
+  logoUrl?: string;
 }
